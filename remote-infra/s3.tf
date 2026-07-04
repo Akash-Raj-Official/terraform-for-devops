a6f5b3bf0c679da01b2e21e3dd9c8396283c107b
Akash Raj <akashraj6202@gmail.com>
2026-07-04 21:33:21 +0530

    Changes in s3

--- remote-infra/s3.tf
+++ remote-infra/s3.tf
@@ -1,8 +1,15 @@
-resource "aws_s3_bucket" "remote-s3" {
-    bucket = "my-remote-s3-bucket0407"
+resource "random_string" "bucket_suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
 
-    tags = {
-        Name = "my-remote-s3-bucket0407"
-        Environment = "Dev"
-    }
+resource "aws_s3_bucket" "remote-s3" {
+  bucket        = "my-remote-s3-bucket-${random_string.bucket_suffix.result}"
+  force_destroy = true
+
+  tags = {
+    Name        = "my-remote-s3-bucket-${random_string.bucket_suffix.result}"
+    Environment = "Dev"
+  }
 }
