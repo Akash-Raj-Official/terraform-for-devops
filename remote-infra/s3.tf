40b328a99bb2b56119a76fe7e22377985e94b16c
Akash Raj <akashraj6202@gmail.com>
2026-07-04 21:29:37 +0530

    Latest change

--- remote-infra/s3.tf
+++ remote-infra/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "remote-s3" {
-    bucket = "my-remote-s3-bucket"
+    bucket = "my-remote-s3-bucket0407"
 
     tags = {
-        Name = "my-remote-s3-bucket"
+        Name = "my-remote-s3-bucket0407"
         Environment = "Dev"
     }
 }
