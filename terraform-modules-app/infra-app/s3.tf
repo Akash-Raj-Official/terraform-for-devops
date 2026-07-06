e747680d4fa37055491fe5e569d63ad4b7781e43
Akash Raj <akashraj6202@gmail.com>
2026-07-06 23:07:24 +0530

    bucket name change

--- terraform-modules-app/infra-app/s3.tf
+++ terraform-modules-app/infra-app/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "remote-s3" {
-  bucket        = "${var.env}-${var.bucket_name}"
+  bucket        = "${var.env}-${var.bucket_name}-akash"
 
   tags = {
-    Name        = "${var.env}-${var.bucket_name}"
+    Name        = "${var.env}-${var.bucket_name}-akash"
     Environment = var.env
   }
 }
